514afe1ec5995a079cbb589439d2a477d1988c69
KruglovaOlga <std094996@ac.eap.gr>
2023-06-19 21:13:36 +0300

    scheduleModule

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,7 +10,47 @@ import { MenuItem } from './user/user.interfaces';
 export class AppComponent {
   title = 'School';
 
+  // userMenu: MenuItem[] = [
+  //   {text: 'List of all users', link: 'user/list'}
+  // ]
+
   userMenu: MenuItem[] = [
-    {text: 'List of all users', link: 'user/list'}
+    {text: 'FindAllStudents', link:'not-implemented-yet'},
+    {text: 'FindAllTeachers', link:'/'},
+    {text: 'FindAllSchedules', link:'/'},
+    {text: 'getById', link:'/'},
+    {text: 'create', link:'/'},  //create student and create teacher
+    {text: 'update', link:'/'},  //update student and teacher
+    {text: 'deleteById', link:'/'},   
+    {text: 'deleteByUsername', link:'/'},
+    {text: 'getUserId', link:'/'},
+    
+  ] 
+
+  studentMenu: MenuItem[] = [
+    
+    {text: 'getById', link:'/'},
+    {text: 'getByUsername', link:'/'},
+    {text: 'create', link:'/'},
+    {text: 'update', link:'/'},
+    {text: 'deleteById', link:'/'},
+    {text: 'deleteByUsername', link:'/'},
+    {text: 'findInstallmentsByUsername', link:'/'},
+    {text: 'getAllGrades', link:'/'},
+    {text: 'getGradesBySemester', link:'/'},
+    {text: 'findStudentsByGroup', link:'/'},
+        
+  ]
+
+  teacherMenu: MenuItem[] = [
+    
+    {text: 'getById', link:'/'},
+    {text: 'getByUsername', link:'/'},
+    {text: 'create', link:'/'},
+    {text: 'update', link:'/'},
+    {text: 'deleteById', link:'/'},
+    {text: 'deleteByUsername', link:'/'},
+    
+    
   ]
 }
--- frontend/src/app/schedule/schedule.module.ts
+++ frontend/src/app/schedule/schedule.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http'
+import { ScheduleService } from './schedule.service';
+import { ScheduleListComponent } from './schedule-list/schedule-list.component';
+
+const routes: Routes = [
+  { path: 'schedule', component: ScheduleListComponent},
+]
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ScheduleListComponent,
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes),
+    HttpClientModule,
+    
+  ],
+  providers:[ScheduleService]
 })
 export class ScheduleModule { }
