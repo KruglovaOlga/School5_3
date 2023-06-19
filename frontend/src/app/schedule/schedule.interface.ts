bb9d8492c6192698ff490aa37b9cc0cb558c8500
KruglovaOlga <std094996@ac.eap.gr>
2023-06-19 21:55:49 +0300

    ScheduleModule

---
+++ frontend/src/app/schedule/schedule.interface.ts
@@ -0,0 +1,32 @@
+export interface MenuItem {
+    text: string
+    link: string
+  }
+  
+
+export interface Schedule {
+    day_of_week: Number,
+        
+      start_time: String,
+        
+      finish_time: String,
+       
+      lesson: String,
+        
+      group:  String,
+        
+      classroom:  String,
+        
+      teacher:  String,
+        
+      students: [
+         String
+          
+      ],
+    };
+    
+    
+    export interface ScheduleAPIList {
+        status: boolean
+        data: Schedule[]
+      }
