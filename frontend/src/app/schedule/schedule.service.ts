e513cb936590a248ff81374d06df00c88e82dae3
KruglovaOlga <std094996@ac.eap.gr>
2023-06-19 21:54:38 +0300

    ScheduleModule

--- frontend/src/app/schedule/schedule.service.ts
+++ frontend/src/app/schedule/schedule.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ScheduleAPIList } from './schedule.interface';
 
-@Injectable({
-  providedIn: 'root'
-})
+const SCHEDULE_API = "http://localhost:3000/api/schedule" 
+
+
+@Injectable()
 export class ScheduleService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+
+
+
+  
+
+  findAll(){
+    return this.http.get<ScheduleAPIList>(`${SCHEDULE_API}/findAll`)
+  }
 }
